d95e6be568a33f189ba45b6466ed668d2671cfc0
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-16 23:09:49 +0100

    function handlePresents created

--- script.js
+++ script.js
@@ -16,9 +16,30 @@ presents.forEach(present => {
 
 boxes.forEach(box => {
 	box.addEventListener('dragover', e => {
-        e.preventDefault()
-        
+		e.preventDefault();
+
 		const isDragged = document.querySelector('.is-dragged');
 		box.appendChild(isDragged);
+
+		handlePresents();
 	});
 });
+
+const handlePresents = () => {
+	const unchosenPresents = presentsBox.querySelectorAll('.present');
+	if (chosenBox.childElementCount > 2) {
+		unchosenPresents.forEach(unchosenPresent => {
+			unchosenPresent.setAttribute('draggable', 'false');
+			unchosenPresent.classList.add('present-disabled');
+		});
+
+		sendBtn.disabled = false;
+	} else {
+		unchosenPresents.forEach(unchosenPresent => {
+			unchosenPresent.setAttribute('draggable', 'true');
+			unchosenPresent.classList.remove('present-disabled');
+		});
+
+		sendBtn.disabled = true;
+	}
+};
